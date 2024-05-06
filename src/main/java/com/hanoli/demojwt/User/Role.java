c0c389a7bd93d4efaa441bed96c5df2642a31bba
hanoli <hanoli@hotmail.com>
2024-05-05 22:10:55 -0600

    first commit

---
+++ src/main/java/com/hanoli/demojwt/User/Role.java
@@ -0,0 +1,6 @@
+package com.hanoli.demojwt.User;
+
+public enum Role {
+    ADMIN,
+    USER  
+}
---
+++ src/main/java/com/hanoli/demojwt/User/UserDTO.java
@@ -0,0 +1,51 @@
+package com.hanoli.demojwt.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    int id;
+    String username;
+    String firstname;
+    String lastname;
+    String country;
+    
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+    
+    
+}
